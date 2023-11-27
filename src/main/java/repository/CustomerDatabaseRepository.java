e83e7a93b8208cde07d15b080c5e662ecaea531f
octavi42 <53470992+octavi42@users.noreply.github.com>
2023-11-27 19:56:49 +0200

    adding create and remove Customer

--- src/main/java/repository/CustomerDatabaseRepository.java
+++ src/main/java/repository/CustomerDatabaseRepository.java
@@ -1,57 +1,45 @@
 package repository;
-import database.Connect_db;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import java.sql.*;
 
 public class CustomerDatabaseRepository {
-    private static Connect_db connection;
-    private void connect() throws SQLException {
-        String url = "jdbc:postgresql://localhost:5432/virtual_guide";
-        String user = "admin";
-        String password = "S3cret";
 
-        connection = new Connect_db(url, user, password);
-    }
+//    private void viewCustomer() throws SQLException {
+//        connect();
+//        String query = "SELECT * FROM Customers";
+//        try (Statement statement = connection.createStatement();
+//             ResultSet resultSet = statement.executeQuery(query)) {
+//            while (resultSet.next()) {
+//                int flightId = resultSet.getInt("flightId");
+//                String airline = resultSet.getString("airline");
+//                String departureTime = resultSet.getString("departureTime");
+//                String arrivalTime = resultSet.getString("arrivalTime");
+//
+//                System.out.println("ID: " + flightId + ", Airline: " + airline + ", departureTime: " + departureTime + ", arrivalTime: " + arrivalTime);
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
-    private void viewCustomer() throws SQLException {
-        connect();
-        String query = "SELECT * FROM Customers";
-        try (Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(query)) {
-            while (resultSet.next()) {
-                int flightId = resultSet.getInt("flightId");
-                String airline = resultSet.getString("airline");
-                String departureTime = resultSet.getString("departureTime");
-                String arrivalTime = resultSet.getString("arrivalTime");
+    public void addCustomer(Connection db, String firstName, String lastName, int age, String email) throws SQLException {
+        String query = "INSERT INTO public.Customer (firstName, lastName, age, email) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement preparedStatement = db.prepareStatement(query)) {
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setInt(3, age);
+            preparedStatement.setString(4, email);
 
-                System.out.println("ID: " + flightId + ", Airline: " + airline + ", departureTime: " + departureTime + ", arrivalTime: " + arrivalTime);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void addCustomer(int flightId, String airline, String departureTime, String arrivalTime) throws SQLException {
-        connect();
-        String query = "INSERT INTO Costumer (flightId, airline, departureTime, arrivalTime) VALUES (?, ?, ?, ?)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            preparedStatement.setInt(1, flightId);
-            preparedStatement.setString(2, airline);
-            preparedStatement.setString(3, departureTime);
-            preparedStatement.setString(4, arrivalTime);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    private void deleteCustomer(int flightId) throws SQLException {
-        connect();
+    public void deleteCustomer(Connection db, int id) throws SQLException {
         String query = "DELETE FROM Flights WHERE flightId = ?";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            preparedStatement.setInt(1, flightId);
+        try (PreparedStatement preparedStatement = db.prepareStatement(query)) {
+            preparedStatement.setInt(1, id);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
--- src/main/java/tests/testDB.java
+++ src/main/java/tests/testDB.java
@@ -4,17 +4,26 @@ package tests;
 import database.Connect_db;
 import org.junit.jupiter.api.Test;
 
+import repository.CustomerDatabaseRepository;
+
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class testDB {
 
     private Connect_db db_connection;
+    private CustomerDatabaseRepository customerDatabaseRepository;
 
     @Test
-    void test_connection() {
+    void test_connection() throws SQLException {
         db_connection = new Connect_db("jdbc:postgresql://localhost:5432/virtual_guide","admin", "S3cret");
         Connection db = db_connection.connect();
         System.out.println(db);
+
+        customerDatabaseRepository = new CustomerDatabaseRepository();
+
+        customerDatabaseRepository.addCustomer(db, "John", "Doe", 25, "jon@main.com");
+        customerDatabaseRepository.deleteCustomer(db, 1);
     }
 
 }
